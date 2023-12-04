576c34560800b2ea5e1c3bc1b6590c23e896022b
Сиди | 西帝 | سيدي <486772+sidi@users.noreply.github.com>
2023-12-04 14:32:26 +0000

    initial setup

---
+++ sid32_2023/app_22xxx/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    mymsg = request.GET.get('message')
+    return render(request, 'home.html', {'user_message': mymsg})
+
+def list(request):
+    mymsg = request.GET.get('message')
+    return render(request, 'home.html', {'user_message': mymsg})
